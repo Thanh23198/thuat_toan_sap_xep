7ca02ae5654f2c498ba43abd1291773f41eff780
Thanh23198 <nguyendinhthanh2311998@gmail.com>
2020-11-20 13:59:30 +0700

    add file

---
+++ chon/index.php
@@ -0,0 +1,22 @@
+<?php
+$mang=array(1,2,3,-1,4,0);
+$sophantu = count($mang);
+    // Lặp để sắp xếp
+    for ($i = 0; $i < $sophantu - 1; $i++)
+    {
+        // Tìm vị trí phần tử lớn nhất
+        $max = $i;
+        for ($j = $i + 1; $j < $sophantu; $j++){
+            if ($mang[$j] > $mang[$max]){
+                $max = $j;
+            }
+        }
+        // Sau khi có vị trí lớn nhất thì hoán vị
+        // với vị trí thứ $i
+        $temp = $mang[$i];
+        $mang[$i] = $mang[$max];
+        $mang[$max] = $temp;
+    }
+for($i=0;$i<$sophantu;$i++){
+    echo $mang[$i].' ';
+}
